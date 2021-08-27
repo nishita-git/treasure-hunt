60dd06ee402cef6bbcdcc1a96c5ac1000b842821
nishita-git <80521868+nishita-git@users.noreply.github.com>
2021-08-27 16:50:47 +0530

    Add files via upload

---
+++ Code.js
@@ -0,0 +1,25 @@
+const accessCode1 = "CANDLE";
+const accessCode2 = "FUNCTION";
+const accessCode3 = "DATABASE";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("E C N D  L A", 100,50)
+    fill("lightblue")
+    text("Hint: I’m tall when I’m young, and I’m short when I’m old. What am I?", 100,70)
+
+    fill("white")
+    textSize(15)
+    text("N C F U O T I N", 700,150)
+    fill("lightblue")
+    text("Hint: Performs a particular task!!", 700,170)
+
+    fill("white")
+    textSize(15)
+    text("A T E D A S B A", 100,250)
+    fill("lightblue")
+    text("Hint: Stores all information !!", 100,270)
+
+}
